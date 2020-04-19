9cc7f903df137b7b72a6dc7b2322bff7638cd1f8
Marshall <kamil.budzik98@gmail.com>
2020-04-19 15:48:48 +0200

    dwa activity i poczatek layoutu

---
+++ app/src/main/java/com/example/ppsm_time_budzik/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ppsm_time_budzik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.checkTimeButton);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TimeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ppsm_time_budzik/TimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.ppsm_time_budzik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+    }
+}
