cfce6ad5ee3a8a21f73e58d7d2defb2e15316063
Marshall <kamil.budzik98@gmail.com>
2020-04-19 17:22:15 +0200

    pobieranie czasu

--- app/src/main/java/com/example/ppsm_time_budzik/MainActivity.java
+++ app/src/main/java/com/example/ppsm_time_budzik/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button b1 = findViewById(R.id.checkTimeButton);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TimeActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void changeActivity(View view) {
+        Intent intent = new Intent(MainActivity.this,TimeActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ppsm_time_budzik/TimeActivity.java
+++ app/src/main/java/com/example/ppsm_time_budzik/TimeActivity.java
@@ -3,12 +3,46 @@ package com.example.ppsm_time_budzik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TimeActivity extends AppCompatActivity {
+
+    private EditText newYorkTime, londonTime, tokyoTime, mobileTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
+        newYorkTime = findViewById(R.id.newYorkValue);
+        londonTime = findViewById(R.id.londonValue);
+        tokyoTime = findViewById(R.id.tokyoValue);
+        mobileTime = findViewById(R.id.mobileTimeValue);
+
+        ScheduledExecutorService e = Executors.newSingleThreadScheduledExecutor();
+        e.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                LocalTime time = LocalTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                mobileTime.setText(time.format(formatter));
+
+                time = LocalTime.now(ZoneId.of("Europe/London"));
+                londonTime.setText(time.format(formatter));
+
+                time = LocalTime.now(ZoneId.of("America/New_York"));
+                newYorkTime.setText(time.format(formatter));
+
+                time = LocalTime.now(ZoneId.of("Asia/Tokyo"));
+                tokyoTime.setText(time.format(formatter));
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+
     }
 }
